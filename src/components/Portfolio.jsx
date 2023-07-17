b031ae45dd69a413a5455d28e067abf5465ae8ea
Mohamud E Hasen <mohmudes4@gmail.com>
2023-07-17 10:02:49 -0500

    added code link setup

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -9,22 +9,26 @@ const Portfolio = () => {
         {
             id: 1,
             src: calenderOne,
-            github: 'https://github.com/mohamud-Hasen/Calendar'
+            github: 'https://github.com/mohamud-Hasen/Calendar',
+            code: 'https://github.com/mohamud-Hasen/Calendar/commit/3abe5bfa27cee54f69b5aae60b1af5f797187d03'
         },
         {
             id: 2,
             src: exchangeOne,
-            github: 'https://github.com/mohamud-Hasen/Exchange-Rate-Calculator'
+            github: 'https://github.com/mohamud-Hasen/Exchange-Rate-Calculator',
+            code: 'https://github.com/mohamud-Hasen/Exchange-Rate-Calculator/commit/ede78149afefdb389f19ec9ca0f41de1c416b9f8'
         },
         {
             id: 3,
             src: thumbnail2,
-            github: 'https://github.com/mohamud-Hasen/dropdown-menu'
+            github: 'https://github.com/mohamud-Hasen/dropdown-menu',
+            code: 'https://github.com/mohamud-Hasen/dropdown-menu/commit/c86b817a48e3f3b9c08fb0c0e63b0b37f2206bf8'
         },
         {
             id: 4,
             src: todoList,
-            github: 'https://github.com/mohamud-Hasen/To-do-list'
+            github: 'https://github.com/mohamud-Hasen/To-do-list',
+            code: 'https://github.com/mohamud-Hasen/To-do-list/commit/b5e1cbaffd6a7a61523ff93541e4c1736b6924ab'
         }
         
     ]
@@ -41,12 +45,12 @@ const Portfolio = () => {
                 <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
 
                 {
-                    portifolios.map(({id, src, github}) => (
+                    portifolios.map(({id, src, github, code}) => (
                         <div  key={id}  className='shadow-md shadow-gray-600 rounded-lg'>
                         <img src={src} alt='' className='rounded-md duration-300 hover:scale-105 cursor-pointer' />
                     <div className='flex items-center justify-center'>
                         <button  className='w-1/2 px-6 py-3 m-4 duration-300 hover:scale-105 hover:underline'><a href={github} target='_blank' rel="noreferrer">Github</a></button>
-                        <button className='w-1/2 px-6 py-3 m-4 duration-300 hover:scale-105 hover:underline'>Code</button>
+                        <button className='w-1/2 px-6 py-3 m-4 duration-300 hover:scale-105 hover:underline'><a href={code}  target='_blank' rel="noreferrer">Code</a></button>
                      </div>
                     </div>
                         
