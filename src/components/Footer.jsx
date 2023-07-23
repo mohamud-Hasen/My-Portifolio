ec2f2ff5816a0a9dcef8c0c8bcc274688faa0499
Mohamud E Hasen <mohmudes4@gmail.com>
2023-07-23 13:29:16 -0500

    minor update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -83,7 +83,7 @@ const Footer = () => {
         }
     ];
   return (
-    <div className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:hidden '>
+    <div className='bg-gradient-to-b from-black to-gray-800 w-full text-white sm:hidden '>
         <ul>
             {links.map(({id, child, style, href, download}) => (
                  <li key={id} className={'flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500'
